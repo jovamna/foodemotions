60387f1ea93fd14f9c7f67631781bb3996cd7328
jovi <jovalidia.medina@gmail.com>
2021-09-26 04:30:55 +0200

    26-09-21 cambios css

---
+++ blog_food/appfood/static/appfood/js/show.js
@@ -0,0 +1,3 @@
+function openwin(url) {
+    window.open(url, "newwindow", "height=9999, width=9999, top=200,left=200 toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no");
+}
