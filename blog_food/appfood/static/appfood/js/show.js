8201afc9ad44844e90295ee8b97a59907cb10191
jovi <jovalidia.medina@gmail.com>
2021-09-27 18:33:01 +0200

    27-09-21-header

--- blog_food/appfood/static/appfood/js/show.js
+++
@@ -1,3 +0,0 @@
-function openwin(url) {
-    window.open(url, "newwindow", "height=9999, width=9999, top=200,left=200 toolbar =no, menubar=no, scrollbars=no, resizable=no, location=no, status=no");
-}
