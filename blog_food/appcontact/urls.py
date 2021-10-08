42b2b9979866df6bba61f143d868ad6ffc2d4334
jovi <jovalidia.medina@gmail.com>
2021-10-08 05:58:23 +0200

    contacto

--- blog_food/appcontact/urls.py
+++ blog_food/appcontact/urls.py
@@ -1,15 +1,12 @@
 from django.urls import path
 from . import views
 
-
 app_name = 'appcontact'
 
 urlpatterns = [
- 
     path('', views.contact, name="contacto"),
-  
+    #path('newsletter/', views.suscribe, name='suscribirse'),
+    #path('validate/', views.validate_email, name='validate_email'),
 ]
 
-
-
-   # Paths del appfood no se pone de inicio contact solo las '' porque ya lo tenemos puesto en la url principal del proyecto
+# Paths del appfood no se pone de inicio contact solo las '' porque ya lo tenemos puesto en la url principal del proyecto
--- blog_food/appcontact/views.py
+++ blog_food/appcontact/views.py
@@ -5,6 +5,12 @@ from .forms import ContactForm
 from apprecetas.models import Kategory
 from apprecetasalzum.models import Category
 
+#from django.http import JsonResponse
+#import re
+#from .models import SubscribedUsers
+#from django.core.mail import send_mail
+#from django.conf import settings
+
 # Create your views here.
 
 
@@ -35,15 +41,53 @@ def contact(request):
             try:
                 email.send()
                 # Todo ha ido bien, redireccionamos a OK
-                return redirect(reverse('contacto') + "?ok")
+                return redirect(reverse('appcontact:contacto') + "?ok")
             except:
                 # Algo no ha ido bien, redireccionamos a FAIL
-                return redirect(reverse('contacto') + "?fail")
+                return redirect(reverse('appcontact:contacto') + "?fail")
 
-    return render(
-        request, "appcontact/contacto.html", {
-            'form': contact_form,
-            'kategory': kategory,
-            'kategories': kategories,
-            'categories': categories,
-        })
+    context = {
+        'form': contact_form,
+        'kategory': kategory,
+        'kategories': kategories,
+        'categories': categories,
+    }
+
+    print(contact_form)
+
+    return render(request, "appcontact/contacto.html", context)
+
+
+#def suscribe(request):
+#if request.method == 'POST':
+#post_data = request.POST.copy()
+#email = post_data.get("email", None)
+#name = post_data.get("name", None)
+#subscribedUsers = SubscribedUsers()
+#subscribedUsers.email = email
+#subscribedUsers.name = name
+#subscribedUsers.save()
+# send a confirmation mail
+#subject = 'NewsLetter Subscription'
+#message = 'Hello ' + name + ', Thanks for subscribing us. You will get notification of latest articles posted on our website. Please do not reply on this email.'
+#email_from = settings.EMAIL_HOST_USER
+#recipient_list = [
+#email,
+#]
+#send_mail(subject, message, email_from, recipient_list)
+#res = JsonResponse({'msg': 'Thanks. Subscribed Successfully!'})
+#return res
+#return render(request, 'suscribirse.html')
+
+#def validate_email(request):
+#email = request.POST.get("email", None)
+#if email is None:
+#res = JsonResponse({'msg': 'Email is required.'})
+#elif SubscribedUsers.objects.get(email=email):
+#res = JsonResponse({'msg': 'Email Address already exists'})
+#elif not re.match(r"^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$",
+#email):
+#res = JsonResponse({'msg': 'Invalid Email Address'})
+#else:
+#res = JsonResponse({'msg': ''})
+#return res
